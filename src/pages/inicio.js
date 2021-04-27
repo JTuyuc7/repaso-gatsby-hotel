09fff2aae5650eefc91aa4948c943081c0fa5c99
JTuyuc7 <50525507+JTuyuc7@users.noreply.github.com>
2021-04-27 00:17:06 -0600

    Limpiar archivos

---
+++ src/pages/inicio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+
+const Inicio = () => {
+    return (  
+        <>
+            <Layout>
+                <h1>Desde inicio</h1>
+            </Layout>
+        </>
+    );
+}
+
+export default Inicio;
